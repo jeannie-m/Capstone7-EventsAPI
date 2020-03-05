a29924e39eb4d5e28438f16a8197c520c0d2b10f
James McDowell <jamesmcdowell1897@gmail.com>
2020-03-05 10:47:35 -0500

    initial commit

---
+++ src/test/java/co/grandcircus/EventsAPI/EventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.grandcircus.EventsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
